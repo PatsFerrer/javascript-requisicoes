ab811b37a0c9d31a539d32c1124b406cec9f9f33
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-05-16 12:36:28 -0300

    try catch do erro na barra de pesquisa, caso pesquise algo que nao existe

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -14,6 +14,10 @@ async function buscarVideo(evento) {
   }
 
   busca.forEach(element => lista.appendChild(construirCard(element.titulo, element.descricao, element.url, element.imagem)));
+
+  if (busca.length == 0) {
+    lista.innerHTML = '<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>'
+  }
 }
 
 const botaoPesquisa = document.querySelector('[data-botao-pesquisa]');
